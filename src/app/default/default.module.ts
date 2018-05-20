e68f65b48dd979fef60c25d9675a517dce3499d5
ericjoel <eric.contreras93@gmail.com>
2018-05-20 12:45:30 -0500

    Adding default, shared an app routing module

---
+++ src/app/default/default.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DefaultRoutingModule } from './default-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DefaultRoutingModule
+  ],
+  declarations: []
+})
+export class DefaultModule { }
